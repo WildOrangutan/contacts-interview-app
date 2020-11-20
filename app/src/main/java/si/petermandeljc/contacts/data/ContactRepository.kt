a30c8bb216790c8360b3a77af1ba9c0f5de619f6
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-11-20 13:39:28 +0100

    Removed ContactRepository unused imports

--- app/src/main/java/si/petermandeljc/contacts/data/ContactRepository.kt
+++ app/src/main/java/si/petermandeljc/contacts/data/ContactRepository.kt
@@ -1,7 +1,5 @@
 package si.petermandeljc.contacts.data
 
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class ContactRepository @Inject constructor(
