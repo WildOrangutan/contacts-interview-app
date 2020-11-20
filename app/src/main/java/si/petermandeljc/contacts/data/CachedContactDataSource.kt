5fc069dbd15435d5a1401c9bf103265ac7a871a2
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-11-20 09:21:41 +0100

    Added CachedDataSource

---
+++ app/src/main/java/si/petermandeljc/contacts/data/CachedContactDataSource.kt
@@ -0,0 +1,15 @@
+package si.petermandeljc.contacts.data
+
+class CachedContactDataSource { // : DataSource
+
+	private val contacts: MutableMap<Uuid,Contact> = mutableMapOf()
+
+	fun save(contact: Contact) {
+		contacts[contact.uuid] = contact
+	}
+
+	fun load(uuid: Uuid) : Contact? {
+		return contacts[uuid]
+	}
+
+}
