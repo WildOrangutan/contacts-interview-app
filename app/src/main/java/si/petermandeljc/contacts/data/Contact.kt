d91dcd2ac39e855b1b2dd1340ff1715fca83e196
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-11-20 13:42:22 +0100

    Added Contact neu() helper

--- app/src/main/java/si/petermandeljc/contacts/data/Contact.kt
+++ app/src/main/java/si/petermandeljc/contacts/data/Contact.kt
@@ -6,4 +6,13 @@ data class Contact(
 	val surname: String,
 	val email: String,
 	val avatarPath: String,
-)
+) {
+
+	companion object Factory {
+		fun neu(name: String, surname: String, email: String, avatarPath: String) : Contact {
+			return Contact(uuid=Uuid.neu(), name=name, surname=surname, email=email,
+				avatarPath=avatarPath)
+		}
+	}
+
+}
