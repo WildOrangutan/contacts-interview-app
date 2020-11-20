18c7069cc3b25fbb68d4b37fa244931082bce926
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-11-20 13:35:21 +0100

    Fixed MainActivity missing entry point annotation

--- app/src/main/java/si/petermandeljc/contacts/ui/MainActivity.kt
+++ app/src/main/java/si/petermandeljc/contacts/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package si.petermandeljc.contacts.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import si.petermandeljc.contacts.databinding.MainActivityBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
